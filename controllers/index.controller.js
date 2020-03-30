b047a949287b7cc7404cde7be9301cfcfa972a84
Vamshi Krishna <matamvamshikrishna@gmail.com>
2020-03-30 09:59:07 +0530

    Get user information along with products if token exists

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,44 +1,93 @@
 const http = require("http")
 
 exports.homepage = function (req, res) {
-    // Cookies that have not been signed
+
     console.log('Cookies: ', req.cookies)
 
-    // Cookies that have been signed
-    //console.log('Signed Cookies: ', req.signedCookies)
-
-    // TODO @vamshi
-    // If cookie exist, get user information
-    // Use promises
+    var promises = []
     var data = {}
-    // Create options
-    const options = {
-        hostname: req.hostname,
-        port: 3000,
-        path: "/products",
-        method: "GET"
+    if (req.cookies.authorization) {
+        var userPromise = new Promise((resolve, reject) => {
+            // Create options
+            const options = {
+                hostname: req.hostname,
+                port: 3000,
+                path: "/users",
+                method: "GET",
+                headers: { authorization: req.cookies.authorization }
+            }
+
+            // Make http request
+            const httpReq = http.request(options, httpRes => {
+                var buff = ""
+                httpRes.on("data", chunks => {
+                    buff += chunks
+                })
+
+                httpRes.on("end", () => {
+                    if (httpRes.statusCode === 200) {
+                        data.user = JSON.parse(buff)
+                        resolve()
+                    }
+                    else {
+                        reject(JSON.parse(buff))
+                    }
+
+                })
+            })
+
+            httpReq.on("error", error => {
+                reject(error)
+            })
+
+            httpReq.end()
+        })
+        promises.push(userPromise)
     }
 
-    // Make http request
-    const httpReq = http.request(options, httpRes => {
-        var buff = ""
-        httpRes.on("data", chunks => {
-            buff += chunks
+
+    var productsPromise = new Promise((resolve, reject) => {
+        // Create options
+        const options = {
+            hostname: req.hostname,
+            port: 3000,
+            path: "/products",
+            method: "GET"
+        }
+
+        // Make http request
+        const httpReq = http.request(options, httpRes => {
+            var buff = ""
+            httpRes.on("data", chunks => {
+                buff += chunks
+            })
+
+            httpRes.on("end", () => {
+                if (httpRes.statusCode === 200) {
+                    data.products = JSON.parse(buff)
+                    resolve()
+                }
+                else {
+                    reject(JSON.parse(buff))
+                }
+            })
         })
 
-        httpRes.on("end", () => {
-            if (httpRes.statusCode === 200) {
-                data.products = JSON.parse(buff)
-            }
-            res.render("index", data)
+        httpReq.on("error", error => {
+            reject(error)
         })
+
+        httpReq.end()
     })
 
-    httpReq.on("error", error => {
+    promises.push(productsPromise)
+    Promise.all(promises).then(() => {
+        console.log(data)
+        res.render("index", data)
+    }).catch(error => {
+        console.log(error)
         res.render("error", error)
     })
-
-    httpReq.end()
 }
 
 exports.signinpage = function (req, res) {
